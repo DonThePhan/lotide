4169fef1da3052d0ceed28eed152700a5915454d
Donny <donthephan@gmail.com>
2021-11-22 22:01:22 +0000

    add reverse.js

---
+++ reverse.js
@@ -0,0 +1,11 @@
+const reverse = function() {
+	for (let i = 0; i < process.argv.length; i++) {
+		if (i > 1) {
+			let reversed = '';
+			for (let j = process.argv[i].length - 1; j >= 0; j--) [ (reversed += process.argv[i][j]) ];
+			console.log(reversed);
+		}
+	}
+};
+
+reverse();
