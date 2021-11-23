f3c4e049887d5c1b968f12a2a333e7162348def3
Donny <donthephan@gmail.com>
2021-11-23 21:55:55 +0000

    add flatten.js

---
+++ flatten.js
@@ -0,0 +1,9 @@
+const flatten = function(array, memo = []) {
+  for (let element of array) {
+    if (Array.isArray(element)) flatten(element, memo);
+    else memo.push(element);
+  }
+  return memo;
+};
+
+console.log(flatten([ 1, 2, [ 3, [ 1, 2, [ 2, [ 1, 5, 7 ], 8, 4 ] ] ], 5, [ 6 ] ]));
