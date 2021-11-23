65192d5ff7c265613fa952e202a5c17d6856c853
Donny <donthephan@gmail.com>
2021-11-23 21:06:52 +0000

    add without.js

---
+++ without.js
@@ -0,0 +1,31 @@
+const without = function (source, itemsToRemove) {
+  let withoutArray = []
+
+  //populate withoutArray with values of source & itemsToRemove that don't match
+  for (let i = 0; i < source.length; i++) {
+    let remove = false
+
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) remove = true
+    }
+    if (!remove) withoutArray.push(source[i])
+  }
+
+  return withoutArray
+
+  // Easier method...
+  // return source.filter(element => {
+  //   if (itemsToRemove.includes(element)) return false
+  //   return true
+  // })
+};
+
+const assertArraysEqual = function(source, itemsToRemove) {
+  console.log('without', without(source, itemsToRemove))
+  console.log('source', source, '\n') //test original source is unmodified
+};
+
+assertArraysEqual([ 1, 2, 3 ], [ 2, 3 ]);
+assertArraysEqual([ 1, 2, 3 ], [ 3 ]);
+assertArraysEqual([ '1', '2', '3' ], [ '3' ]);
+assertArraysEqual([ '1', '2', '3' ], [ '1', 3 ]);
