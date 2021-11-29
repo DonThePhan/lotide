5e1541db45e73a7514a43d2ee67baae8282f3051
Donny <donthephan@gmail.com>
2021-11-29 19:36:17 +0000

    integrate Mocha into testHead & add .gitignore

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,24 @@
-const head = require('../head')
-const assertEqual = require('../assertEqual')
+const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hi", "Lighthouse", "Labs"]), "Hello");
+// const assertEqual = require('../assertEqual');
+// assertEqual(head([ 5, 6, 7 ]), 5);
+// assertEqual(head([]), 5);
+// assertEqual(head([ 'Hello', 'Lighthouse', 'Labs' ]), 'Hello');
+// assertEqual(head([ 'Hi', 'Lighthouse', 'Labs' ]), 'Hello');
+
+const assert = require('chai').assert;
+
+describe('#head', () => {
+  it('should return 5 for [5, 6, 7]', () => {
+    assert.strictEqual(head([ 5, 6, 7 ]), 5);
+  });
+  it('should return undefined for []', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it(`should return 'hello' for [ 'Hello', 'Lighthouse', 'Labs' ]`, () => {
+    assert.strictEqual(head([ 'Hello', 'Lighthouse', 'Labs' ]), 'Hello');
+  });
+  it(`should return 'Hi' for [ 'Hi', 'Lighthouse', 'Labs' ]`, () => {
+    assert.strictEqual(head([ 'Hi', 'Lighthouse', 'Labs' ]), 'Hi');
+  });
+});
